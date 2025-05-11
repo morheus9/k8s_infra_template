549e016999b2740e85dbdde9e51e7ae779489b30
Vyacheslav <nodegopher@gmail.com>
2025-05-11 16:41:38 +0300

    f

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "git::https://github.com/terraform-yc-modules/terraform-yc-vpc.git?ref=1.0.9 "
+  source = "git::https://github.com/terraform-yc-modules/terraform-yc-vpc.git?ref=1.0.9"
 
   create_nat_gw = true
   network_name  = "k8s-network--${terraform.workspace}"
@@ -21,7 +21,7 @@ module "network" {
 }
 
 module "kube" {
-  source = "git::https://github.com/terraform-yc-modules/terraform-yc-kubernetes.git?ref=1.1.2 "
+  source = "git::https://github.com/terraform-yc-modules/terraform-yc-kubernetes.git?ref=1.1.2"
 
   node_groups_defaults    = var.node_groups_defaults
   enable_outgoing_traffic = var.enable_outgoing_traffic
@@ -85,7 +85,7 @@ module "kube" {
 #     Modules for managed k8s
 
 module "addons" {
-  source = "git::https://github.com/terraform-yc-modules/terraform-yc-kubernetes-marketplace.git?ref=1.0.0 "
+  source = "git::https://github.com/terraform-yc-modules/terraform-yc-kubernetes-marketplace.git?ref=1.0.0"
 
   cluster_id = module.kube.cluster_id
 
